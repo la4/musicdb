5f0889aee16cb367554f7a85bdeb17a079753a9a
Vadim <vadim.sidorov@coderivium.com>
2015-11-09 12:58:25 +0200

    Added DBHelper (SQLiteOpenHelper).

---
+++ app/src/main/java/com/coderivium/sidorov/vadim/musicdb/DBHelper.java
@@ -0,0 +1,30 @@
+package com.coderivium.sidorov.vadim.musicdb;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String LOG_TAG = DBHelper.class.getSimpleName();
+
+    public DBHelper(Context context) {
+        super(context, "myDB", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(LOG_TAG, "--- onCreate database ---");
+
+        db.execSQL("create table mytable ("
+                + "id integer primary key autoincrement,"
+                + "name text,"
+                + "email text" + ");");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
